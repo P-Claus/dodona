7778541e17905bcffa034a2f95e5ca3f469e2779
Pieter Claus <pieter@MacBookanPieter.lan>
2022-02-20 11:38:48 +0100

    started loop control exercise

--- De Programmeursleerling (Pieter Spronck)/for_loop_ex_1.py
+++
@@ -1,2 +0,0 @@
-for x in range(21, 3, -3):
-    print(x)
---
+++ De Programmeursleerling (Pieter Spronck)/loop_control_ex_1.py
@@ -0,0 +1,12 @@
+sum = 0
+
+for i in ( 12, 4, 3, 33, -2, -5, 7, 0, 22, 4 ):
+    if i == 0:
+        break
+    if i < 0:
+        continue
+    else: 
+        sum =+ i
+        
+
+print('Done')

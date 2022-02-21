756dee558989308612a6d5d04104f5629b273112
Pieter Claus <pieter@MacBook-van-Pieter.local>
2022-02-21 21:53:42 +0100

    finished loop control exercise

--- De Programmeursleerling (Pieter Spronck)/loop_control_ex_1.py
+++ De Programmeursleerling (Pieter Spronck)/loop_control_ex_1.py
@@ -1,12 +1,15 @@
 sum = 0
 
-for i in ( 12, 4, 3, 33, -2, -5, 7, 0, 22, 4 ):
+for i in ( 12, 4, 3, 33, -2, -5, 7, 22, 4 ):
+    
     if i == 0:
         break
     if i < 0:
         continue
-    else: 
-        sum =+ i
+    sum += i
+
+else: 
+    print(sum)
         
 
 print('Done')

0ce9fd68fbad6f0c6ac09b1de15e9083c28eba5c
Pieter Claus <pieter@MacBook-van-Pieter.local>
2022-03-07 21:51:09 +0100

    finished functions

--- De Programmeursleerling (Pieter Spronck)/what_is_wrong.py
+++ De Programmeursleerling (Pieter Spronck)/what_is_wrong.py
@@ -1,6 +1,6 @@
 def area_of_triangle( bottom, height ):
     area = 0.5 * bottom * height
-    return bottom, height, area
+    string = "The area of a triangle with a bottom of {} and a height of {} is {}".format(bottom, height, area)
+    return string
 
-bottom, height, area = area_of_triangle(4.5, 1.0)
-print("The area of a triangle with a bottom of {} and a height of {} is {}".format(bottom, height, area))
+print(area_of_triangle(4.5, 1.0))

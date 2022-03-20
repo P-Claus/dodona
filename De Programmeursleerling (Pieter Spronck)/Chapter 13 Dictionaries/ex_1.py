26d828e1460dfbcf5ff5035e96dcdfc3e94eead8
Pieter Claus <pieter@MacBook-van-Pieter.local>
2022-03-20 22:07:47 +0100

    cleanup + worked on dictionaries

---
+++ De Programmeursleerling (Pieter Spronck)/Chapter 13 Dictionaries/ex_1.py
@@ -0,0 +1,7 @@
+wordlist = ["apple","durian","banana","durian","apple","cherry",
+    "cherry","mango","apple","apple","cherry","durian","banana",
+    "apple","apple","apple","apple","banana","apple"]
+
+dict_words = {'apple':9, 'durian':3, 'banana':4, 'mango': 3}
+
+print(dict_words)
---
+++ De Programmeursleerling (Pieter Spronck)/Chapter 13 Dictionaries/ex_2.py
@@ -0,0 +1,10 @@
+english_dutch = { "last":"laatst", "week":"week", "the":"de",
+    "royal":"koninklijk", "festival":"feest", "hall":"hal", "saw":
+    "zaag", "first":"eerst", "performance":"optreden", "of":"van", 
+    "a":"een", "new":"nieuw", "symphony":"symphonie", "by":"bij", 
+    "one":"een", "world":"wereld", "leading":"leidend", "modern":
+    "modern", "composer":"componist", "composers":"componisten", 
+    "two":"twee", "shed":"schuur", "sheds":"schuren" }
+
+for word in english_dutch:
+    print('{}:{}'.format(word, english_dutch[word]))

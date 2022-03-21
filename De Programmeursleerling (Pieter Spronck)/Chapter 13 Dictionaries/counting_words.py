d55b833308e4f0af54440a422aa0cca2d70393c5
Pieter Claus <pieter@MacBook-van-Pieter.local>
2022-03-21 18:38:38 +0100

    finished counting words exercise

--- De Programmeursleerling (Pieter Spronck)/Chapter 13 Dictionaries/counting_words.py
+++ De Programmeursleerling (Pieter Spronck)/Chapter 13 Dictionaries/counting_words.py
@@ -1,15 +1,12 @@
-text = 'How much wood would a woodchuck chuck if a woodchuck could chuck wood? He would chuck, he would, as much as he could, and chuck as much as a woodchuck would If a woodchuck could chuck wood.'
-s = text.lower()
-ss = s.strip()
-sss = ss.split()
-dict = {}
 
-for word in sss:
-    if word in dict:
-        dict[word] += 1 
-    else:
-        dict[word] = 1
-
-print(dict)
-    
-
+def word_count(s):
+    s_lower = s.lower()
+    s_strip = s_lower.strip('?,.')
+    s_split = s_strip.split()
+    dict = {}  
+    for word in s_split:
+        if word in dict:
+            dict[word] += 1 
+        else:
+            dict[word] = 1
+    return dict
--- De Programmeursleerling (Pieter Spronck)/Chapter 13 Dictionaries/try.py
+++ De Programmeursleerling (Pieter Spronck)/Chapter 13 Dictionaries/try.py
@@ -1,6 +1,16 @@
 text = 'How much wood would a woodchuck chuck if a woodchuck could chuck wood? He would chuck, he would, as much as he could, and chuck as much as a woodchuck would If a woodchuck could chuck wood.'
+dict = {}
 s = text
 
-x = s.split()
+x = s.lower()
+n = x.split()
 
-print(x)
+for wrd in n:
+    o = wrd.strip('?,.')
+    if o in dict:
+        dict[o] += 1 
+    else:
+        dict[o] = 1
+
+print(dict)
+

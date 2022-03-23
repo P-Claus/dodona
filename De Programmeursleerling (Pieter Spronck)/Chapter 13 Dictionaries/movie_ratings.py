68c21ed365dffd74cbb22cd8c7951cf6cc76893e
Pieter Claus <pieter@MacBook-van-Pieter.local>
2022-03-23 21:55:25 +0100

    continued movie ratings exercise

--- De Programmeursleerling (Pieter Spronck)/Chapter 13 Dictionaries/movie_ratings.py
+++ De Programmeursleerling (Pieter Spronck)/Chapter 13 Dictionaries/movie_ratings.py
@@ -1,10 +1,15 @@
-movies_list = [('Monty Python and the Holy Grail', (9, 10, 9.5, 8.5, 3, 7.5, 8)), ("Monty Python's Life of Brian", (10, 10, 0, 9, 1, 8, 7.5, 8, 6, 9)), ("Monty Python's Meaning of Life", (7, 6, 5)), ('And Now For Something Completely Different', (6, 5, 6, 6)),]
+movies_list = [
+...     ('Monty Python and the Holy Grail', (9, 10, 9.5, 8.5, 3, 7.5, 8)),
+...     ("Monty Python's Life of Brian", (10, 10, 0, 9, 1, 8, 7.5, 8, 6, 9)),
+...     ("Monty Python's Meaning of Life", (7, 6, 5)),
+...     ('And Now For Something Completely Different', (6, 5, 6, 6)),
+... ]
 
-ratings = {}
+def list2dict(s):
+    ratings = {}
+    for s in movies_list:
+        if s not in ratings:
+            ratings += s
+    value = ratings[s]
+    return value
 
-
-
-
-
-
-print(ratings)
